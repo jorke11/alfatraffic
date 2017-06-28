5e91d8ca0ff5a38e953072533d80cf64cee92d3d
jorge debian <jpinedom@hotmail.com>
2017-06-28 11:57:37 -0500

    fixed file datavase

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,11 +68,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-107-21-109-15.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'dgb5mqqp2v834'),
+            'username' => env('DB_USERNAME', 'sevprrsddylvgq'),
+            'password' => env('DB_PASSWORD', 'd64b90f2a67abfc9536760f6912a6845f5d285b3538997919890d921fc8f7041'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
